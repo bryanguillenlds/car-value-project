40a7b8d0f5a7178ce447737737b5f209eb57d53d
Bryan Guillen <bryan.guillen.lds@gmail.com>
2022-03-01 21:08:19 -0700

    Created a Global Interceptor to assign a current user to the request so the CurrentUser Param Decorator can get it.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,5 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,6 +10,15 @@ import { User } from './user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService],
+  providers: [
+    UsersService,
+    AuthService,
+    //config obj to set up
+    //a global interceptor
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
